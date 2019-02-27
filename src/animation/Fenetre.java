8771fc281d8f05722b1a4b5e4e150d8de9c57216
geo-toma <nmenageorges02@gmail.com>
2019-02-27 09:51:11 +0100

    Boite de dialog

--- src/animation/Fenetre.java
+++ src/animation/Fenetre.java
@@ -9,47 +9,48 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import interactionButton.Bouton;
 
 public class Fenetre extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
 	private Panneau pan = new Panneau();
 	private Bouton bouton = new Bouton("Go");
 	private Bouton bouton1 = new Bouton("Stop");
 	private JPanel bcontent = new JPanel();
 	private JPanel content = new JPanel();
-	private String[] tab = {"ROND" ,"CARRE" ,"TRIANGLE" ,"ETOILE"};
+	private String[] tab = { "ROND", "CARRE", "TRIANGLE", "ETOILE" };
 	private JComboBox<String> combo = new JComboBox<>(tab);
 	private JCheckBox box = new JCheckBox("Morphing");
 	private JLabel label = new JLabel("Choix de forme");
-	
-	int x,y;
+
+	int x, y;
 	boolean animated = true;
 	boolean backX = false, backY = false;
-	
+
 	public Fenetre() {
 		this.setTitle("animation");
 		this.setSize(300, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		
+
 		bouton.addActionListener(new BoutonListener());
 		bouton.setEnabled(false);
 		bouton1.addActionListener(new Bouton2Listener());
-		
+
 		bcontent.add(bouton);
 		bcontent.add(bouton1);
-		
+
 		JPanel top = new JPanel();
 		combo.addActionListener(new ItemAction());
 		box.addActionListener(new BoxAction());
 		top.add(label);
 		top.add(combo);
 		top.add(box);
-		
+
 		content.setBackground(Color.WHITE);
 		content.setLayout(new BorderLayout());
 		content.add(top, BorderLayout.NORTH);
@@ -63,26 +64,26 @@ public class Fenetre extends JFrame {
 	private void go() {
 		x = pan.getPosX();
 		y = pan.getPosY();
-		while(animated) {
-			if(x < 1)
+		while (animated) {
+			if (x < 1)
 				backX = false;
-			if(x > pan.getWidth()-pan.getDrawSize())
+			if (x > pan.getWidth() - pan.getDrawSize())
 				backX = true;
-			if(y < 1)
+			if (y < 1)
 				backY = false;
-			if(y > pan.getHeight()-pan.getDrawSize())
+			if (y > pan.getHeight() - pan.getDrawSize())
 				backY = true;
-			
-			if(backX)
+
+			if (backX)
 				pan.setPosX(--x);
 			else
 				pan.setPosX(++x);
-			
-			if(backY)
+
+			if (backY)
 				pan.setPosY(--y);
 			else
 				pan.setPosY(++y);
-			
+
 			pan.repaint();
 			try {
 				Thread.sleep(50);
@@ -91,60 +92,65 @@ public class Fenetre extends JFrame {
 			}
 		}
 	}
-	
-	public void name() {
-		
-	}
-	
-	class BoutonListener implements ActionListener{
+
+	class BoutonListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			animated = true;
-			bouton.setEnabled(false);
-			bouton1.setEnabled(true);
-			Thread thread = new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-					go();
-				}
-			});
-			thread.start();
+			int option = JOptionPane.showConfirmDialog(null, "Voulez vous lancer l'animation ?",
+					"lancement de l'animation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+			if (option == JOptionPane.OK_OPTION) {
+				animated = true;
+				bouton.setEnabled(false);
+				bouton1.setEnabled(true);
+				Thread thread = new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						go();
+					}
+				});
+				thread.start();
+			}
 		}
-		
+
 	}
-	
-	class Bouton2Listener implements ActionListener{
+
+	class Bouton2Listener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			animated = false;
-			bouton.setEnabled(true);
-			bouton1.setEnabled(false);
+			int option = JOptionPane.showConfirmDialog(null, "Voulez vous arretez l'animatoin", "arret de l'animation",
+					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+			if (option != JOptionPane.CANCEL_OPTION && option != JOptionPane.CLOSED_OPTION
+					&& option != JOptionPane.NO_OPTION) {
+				animated = false;
+				bouton.setEnabled(true);
+				bouton1.setEnabled(false);
+			}
 		}
-		
+
 	}
-	
-	class ItemAction implements ActionListener{
+
+	class ItemAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			pan.setForme(combo.getSelectedItem().toString());
 		}
-		
+
 	}
-	
-	class BoxAction implements ActionListener{
+
+	class BoxAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(((JCheckBox)e.getSource()).isSelected())
+			if (((JCheckBox) e.getSource()).isSelected())
 				pan.setMorph(true);
 			else
 				pan.setMorph(false);
 		}
-		
+
 	}
 
 }
--- src/animation/Main.java
+++ src/animation/Main.java
@@ -3,8 +3,7 @@ package animation;
 public class Main {
 
 	public static void main(String[] args) {
-		Fenetre fen = new Fenetre();
-		fen.name();
+		new Fenetre();
 	}
 
 }
